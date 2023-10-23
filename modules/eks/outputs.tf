e6d13d4ca9357dc904aff2d8f9227b21947f5fe7
lomasz <lukasz.tomaszewski89@gmail.com>
2023-10-23 09:49:45 +0200

    fix providing k8s auth in helm

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -17,3 +17,9 @@ output "cluster_endpoint" {
   description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
+
+output "cluster_certificate_authority_data" {
+  description = "EKS base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
--- providers.tf
+++ providers.tf
@@ -3,9 +3,7 @@ terraform {
     bucket = "my-terraform-state-bucket"
     key    = "state/terraform.tfstate"
   }
-}
 
-terraform {
   required_version = "= 1.5.7"
 
   required_providers {
@@ -21,6 +19,12 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }
